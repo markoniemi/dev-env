3b5956cb96726cdb1b0958fb182d9a5ab541f9be
Marko Niemi <marko.j.niemi@gmail.com>
2019-09-06 21:37:41 +0300

    enable maven in dev-env.

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -2,7 +2,7 @@
 source /vagrant/vagrant/install-docker-compose.sh
 source /vagrant/vagrant/disable-docker-tls.sh
 
-provision() {
-  install_docker_compose
-  disable_docker_tls
-}
+#provision() {
+#  install_docker_compose
+#  disable_docker_tls
+#}
