50ca49f69220c07a242ceef2b21e1213b3a3a86d
Marko Niemi <marko.j.niemi@gmail.com>
2019-09-08 15:23:46 +0300

    autocrl=false in .gitconfig. Add .bashrc to install.sh. Run install.sh in Vagrantfile.

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -2,7 +2,8 @@
 source /vagrant/vagrant/install-docker-compose.sh
 source /vagrant/vagrant/disable-docker-tls.sh
 
-#provision() {
+provision() {
+  source ~/.bashrc
 #  install_docker_compose
 #  disable_docker_tls
-#}
+}
